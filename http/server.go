9e3c32bff59a95ccd4a52083f7c8624ffefca540
ReotoKawaguchi0010 <pnxde6mxu@i.softbank.jp>
2021-06-20 10:47:27 +0900

    server

--- http/server.go
+++ http/server.go
@@ -7,20 +7,25 @@ import (
 )
 
 
+func configHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
+		fn(w, r)
+	}
+}
 
 func Handler(w http.ResponseWriter, r *http.Request){
 	fmt.Printf("reuqest method %s\n", r.Method)
 	fmt.Printf("reuqest host%s\n", r.Host)
-	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
-
-
-	fmt.Fprintf(w, "test")
+	_, err := fmt.Fprintf(w, "test")
+	if err != nil {
+		return
+	}
 }
 
 
 func Router(){
 	fmt.Println("run http-server http://localhost:8000")
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/", configHandler(Handler))
 	log.Fatal(http.ListenAndServe(":8000", nil))
-
 }
